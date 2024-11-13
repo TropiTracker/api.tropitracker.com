8a7869267d120f0833cca1128c779759ed2d0d44
CGray1234 <cjgray1208@gmail.com>
2024-11-12 23:08:15 -0500

    idek what im doing im just hoping this works

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const port = 8484;
 outlooks.functions.init();
 
 app.get('/', async (req, res) => {
-    const outlookData = await outlooks.functions.getAtlanticOutlook();
-    console.log(outlookData)
-    res.json({ data: outlookData });
+    const atlanticOutlook = await outlooks.functions.getAtlanticOutlook();
+    const eastPacificOutlook = await outlooks.functions.getEastPacificOutlook();
+    const centralPacificOutlook = await outlooks.functions.getCentralPacificOutlook();
+    res.json({
+        outlooks: {
+            atlanticOutlook, 
+            eastPacificOutlook, 
+            centralPacificOutlook
+        }
+    });
 })
 
 app.listen(port, () => {
