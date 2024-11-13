d23e35067e3cf9e5e05e271a001554c9bb64181b
CGray1234 <cjgray1208@gmail.com>
2024-11-13 18:07:37 -0500

    Auto write to json file (will be used later)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express');
 const outlooks = new (require('./lib/outlooks.js'))
 const app = express();
@@ -5,17 +6,22 @@ const port = 8484;
 
 outlooks.functions.init();
 
-app.get('/', async (req, res) => {
+app.get('/index.json', async (req, res) => {
     const atlanticOutlook = await outlooks.functions.getAtlanticOutlook();
     const eastPacificOutlook = await outlooks.functions.getEastPacificOutlook();
     const centralPacificOutlook = await outlooks.functions.getCentralPacificOutlook();
-    res.json({
+
+    const json = {
         outlooks: {
-            atlanticOutlook, 
-            eastPacificOutlook, 
-            centralPacificOutlook
+            atlanticOutlook: atlanticOutlook, 
+            eastPacificOutlook: eastPacificOutlook, 
+            centralPacificOutlook: centralPacificOutlook
         }
-    });
+    }
+
+    fs.writeFileSync('index.json', JSON.stringify(json, null, 2));
+
+    res.json(json); // ngl i dont even know if this is necessary
 })
 
 app.listen(port, () => {
