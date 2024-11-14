cfd77fd605e9de514e3593fe84ac1426867ded29
CGray1234 <cjgray1208@gmail.com>
2024-11-14 14:29:22 -0500

    Individual storm types support

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ outlooks.functions.init();
 cyclones.functions.init();
 
 var json;
+var jsonHurricanes;
 var jsonTropicalStorms;
+var jsonTropicalDepressions;
 
 async function loadApi() {
     const atlanticOutlook = await outlooks.functions.getAtlanticOutlook();
@@ -32,6 +34,18 @@ async function loadApi() {
 
 loadApi();
 
+async function loadHurricanes() {
+    const activeStorms = await cyclones.functions.getHurricanes();
+
+    jsonHurricanes = {
+        activeStorms
+    }
+
+    fs.writeFileSync('hurricanes.json', JSON.stringify(jsonHurricanes, null, 2));
+}
+
+loadHurricanes();
+
 async function loadTropicalStorms() {
     const activeStorms = await cyclones.functions.getTropicalStorms();
 
@@ -44,14 +58,36 @@ async function loadTropicalStorms() {
 
 loadTropicalStorms();
 
+async function loadTropicalDepressions() {
+    const activeDepressions = await cyclones.functions.getTropicalDepressions();
+    const potentialCyclones = await cyclones.functions.getPotentialCyclones();
+
+    jsonTropicalDepressions = {
+        activeDepressions,
+        potentialCyclones
+    }
+
+    fs.writeFileSync('tropical-depressions.json', JSON.stringify(jsonTropicalDepressions, null, 2));
+}
+
+loadTropicalDepressions();
+
 // De-note when testing
 
 // app.get('/index.json', async (req, res) => {
 //     res.json(json);
 // })
 
+// app.get('/hurricanes.json', async (req, res) => {
+//     res.json(jsonHurricanes);
+// })
+
 // app.get('/tropical-storms.json', async (req, res) => {
 //     res.json(jsonTropicalStorms);
+// })
+
+// app.get('/tropical-depressions.json', async (req, res) => {
+//     res.json(jsonTropicalDepressions);
 // })
 
 // app.listen(port, () => {
