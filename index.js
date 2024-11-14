791d06fe4055ff7158397d8e6b569cceeb66e28b
CGray1234 <cjgray1208@gmail.com>
2024-11-13 19:51:53 -0500

    Fix gh actions errors, add cone tracks & satellite gifs

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-const fs = require('fs')
+const fs = require('fs');
 const express = require('express');
-const outlooks = new (require('./lib/outlooks.js'))
-const cyclones = new (require('./lib/cyclones.js'))
+const outlooks = new (require('./lib/outlooks.js'));
+const cyclones = new (require('./lib/cyclones.js'));
 const app = express();
 const port = 8484;
 
 outlooks.functions.init();
 cyclones.functions.init();
 
-const atlanticOutlook = await outlooks.functions.getAtlanticOutlook();
-const eastPacificOutlook = await outlooks.functions.getEastPacificOutlook();
-const centralPacificOutlook = await outlooks.functions.getCentralPacificOutlook();
+var json;
 
-const activeStorms = await cyclones.functions.getAllStorms();
+async function loadApi() {
+    const atlanticOutlook = await outlooks.functions.getAtlanticOutlook();
+    const eastPacificOutlook = await outlooks.functions.getEastPacificOutlook();
+    const centralPacificOutlook = await outlooks.functions.getCentralPacificOutlook();
 
-const json = {
-    outlooks: {
-        atlanticOutlook: atlanticOutlook, 
-        eastPacificOutlook: eastPacificOutlook, 
-        centralPacificOutlook: centralPacificOutlook
-    },
-    activeStorms
+    const activeStorms = await cyclones.functions.getAllStorms();
+
+    json = {
+        outlooks: {
+            atlanticOutlook: atlanticOutlook, 
+            eastPacificOutlook: eastPacificOutlook, 
+            centralPacificOutlook: centralPacificOutlook
+        },
+        activeStorms
+    }
+
+    fs.writeFileSync('index.json', JSON.stringify(json, null, 2));
 }
 
-fs.writeFileSync('index.json', JSON.stringify(json, null, 2));
+loadApi();
 
 app.get('/index.json', async (req, res) => {
     res.json(json); // ngl i dont even know if this is necessary
