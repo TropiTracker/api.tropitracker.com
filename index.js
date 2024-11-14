39dfd9f3f7ef394a29d623a9453c9be595edf963
CGray1234 <cjgray1208@gmail.com>
2024-11-13 19:12:16 -0500

    add cyclones and json file writing

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const fs = require('fs')
 const express = require('express');
 const outlooks = new (require('./lib/outlooks.js'))
+const cyclones = new (require('./lib/cyclones.js'))
 const app = express();
 const port = 8484;
 
 outlooks.functions.init();
+cyclones.functions.init();
+
+const atlanticOutlook = await outlooks.functions.getAtlanticOutlook();
+const eastPacificOutlook = await outlooks.functions.getEastPacificOutlook();
+const centralPacificOutlook = await outlooks.functions.getCentralPacificOutlook();
+
+const activeStorms = await cyclones.functions.getAllStorms();
+
+const json = {
+    outlooks: {
+        atlanticOutlook: atlanticOutlook, 
+        eastPacificOutlook: eastPacificOutlook, 
+        centralPacificOutlook: centralPacificOutlook
+    },
+    activeStorms
+}
+
+fs.writeFileSync('index.json', JSON.stringify(json, null, 2));
 
 app.get('/index.json', async (req, res) => {
-    const atlanticOutlook = await outlooks.functions.getAtlanticOutlook();
-    const eastPacificOutlook = await outlooks.functions.getEastPacificOutlook();
-    const centralPacificOutlook = await outlooks.functions.getCentralPacificOutlook();
-
-    const json = {
-        outlooks: {
-            atlanticOutlook: atlanticOutlook, 
-            eastPacificOutlook: eastPacificOutlook, 
-            centralPacificOutlook: centralPacificOutlook
-        }
-    }
-
-    fs.writeFileSync('index.json', JSON.stringify(json, null, 2));
-
     res.json(json); // ngl i dont even know if this is necessary
 })
 
